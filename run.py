f838813f7b740342e1c8765766a9544796a681c6
benbarker04 <benbarker074@gmail.com>
2024-03-26 13:16:13 +0000

    add templates and run.py so page can be viewed

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug = True
+    )
